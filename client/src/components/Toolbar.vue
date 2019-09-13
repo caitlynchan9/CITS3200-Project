f1b268d03e4532b0e5e88ba324288b401e643e7e
Tim Eitel <Timothy.Eitel@gmail.com>
2019-09-13 23:47:47 +0800

    About to try different v-list in drawer

--- client/src/components/Toolbar.vue
+++ client/src/components/Toolbar.vue
@@ -11,10 +11,16 @@
     </v-toolbar>
 
     <v-navigation-drawer v-model="drawer" color="grey lighten-1">
-      <v-btn block>Home</v-btn>
-      <v-btn block>Competencies</v-btn>
-      <v-btn block>Search</v-btn>
-      <v-btn block>Logout</v-btn>
+      <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-tile-action>
+              <v-icon> {{ link.icon }} </v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+              <v-list-tile-title> {{ link.text }} </v-list-tile-title>
+          </v-list-tile-content>
+
+      </v-list-tile>
     </v-navigation-drawer>
   </nav>
 </template>
@@ -23,7 +29,12 @@
 export default {
   data() {
     return {
-      drawer: false
+      drawer: false,
+      links: [
+          {icon: 'fas fa-lock', text: 'Home', route: '/'},
+          {icon: 'fas fa-edit', text: 'Competencies', route: '/competencies'},
+          {icon: 'fas fa-list', text: 'Publish', route: '/publish'},
+      ]
     }
   }
 }
