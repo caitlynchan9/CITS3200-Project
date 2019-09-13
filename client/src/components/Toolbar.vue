a96bfb6aa9db3c2d4a82818962a99898ff84e9ca
Tim Eitel <Timothy.Eitel@gmail.com>
2019-09-14 00:12:46 +0800

    Updated side drawer

--- client/src/components/Toolbar.vue
+++ client/src/components/Toolbar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <v-toolbar flat app color="indigo darken-4" height="150">
-        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>
         <a>
           <img src="@/assets/uwa-logo.png" alt />
@@ -11,31 +11,31 @@
     </v-toolbar>
 
     <v-navigation-drawer v-model="drawer" color="grey lighten-1">
-      <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
-          <v-list-tile-action>
-              <v-icon> {{ link.icon }} </v-icon>
-          </v-list-tile-action>
-
-          <v-list-tile-content>
-              <v-list-tile-title> {{ link.text }} </v-list-tile-title>
-          </v-list-tile-content>
-
-      </v-list-tile>
+      <v-list rounded>
+        <v-list-item-group v-model="item" color="indigo darken-4">
+          <v-list-item v-for="(item, i) in items" :key="i" router :to="items.route">
+            <v-list-item-icon>
+              <v-icon v-text="item.icon"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.text"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
     </v-navigation-drawer>
   </nav>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      drawer: false,
-      links: [
-          {icon: 'fas fa-lock', text: 'Home', route: '/'},
-          {icon: 'fas fa-edit', text: 'Competencies', route: '/competencies'},
-          {icon: 'fas fa-list', text: 'Publish', route: '/publish'},
-      ]
-    }
+  export default {
+    data: () => ({
+      item: 1,
+      items: [
+        { text: 'Home', icon: 'mdi-clock', route: '/' },
+        { text: 'Competencies', icon: 'mdi-account', route: '/competencies' },
+        { text: 'Search', icon: 'mdi-flag', route: '/publish' },
+      ],
+    }),
   }
-}
 </script>
