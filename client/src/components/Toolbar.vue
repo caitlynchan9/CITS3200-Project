1b8ee9db5d40fef69612098b77671c8ae8314c56
Tim Eitel <Timothy.Eitel@gmail.com>
2019-09-15 10:24:28 +0800

    Removed redundant icons and added toolbar title

--- client/src/components/Toolbar.vue
+++ client/src/components/Toolbar.vue
@@ -1,16 +1,16 @@
 <template>
   <nav>
     <v-toolbar flat app color="indigo darken-4" height="150">
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>
         <a>
           <img src="@/assets/uwa-logo.png" alt />
         </a>
       </v-toolbar-title>
-      <v-toolbar-title right>Logout</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-title class="white--text">UWA Acquisition</v-toolbar-title>
     </v-toolbar>
 
-    <v-navigation-drawer v-model="drawer" color="grey lighten-1">
+    <v-navigation-drawer color="grey lighten-1">
       <v-list rounded>
         <v-list-item-group v-model="item" color="indigo darken-4">
           <v-list-item v-for="(item, i) in items" :key="i" router :to="items.route">
@@ -27,6 +27,7 @@
   </nav>
 </template>
 
+
 <script>
   export default {
     data: () => ({
@@ -35,6 +36,7 @@
         { text: 'Home', icon: 'mdi-clock', route: '/' },
         { text: 'Competencies', icon: 'mdi-account', route: '/competencies' },
         { text: 'Search', icon: 'mdi-flag', route: '/publish' },
+        { text: 'Logout', icon: 'mdi-flag', route: '/logout' },
       ],
     }),
   }
