dc3ea8668e0e6dbbe36d9f24941d27638146b897
Tim Eitel <Timothy.Eitel@gmail.com>
2019-09-15 13:22:21 +0800

    Adding competency cards - will change to 3 columns

--- client/src/views/Competencies.vue
+++ client/src/views/Competencies.vue
@@ -1,9 +1,53 @@
 <template>
-    <h1>Competencies</h1>
+  <v-card>
+
+    <v-container class="pa-2" fluid>
+      <v-row>
+        <v-col>
+          <v-card color="#385F73" dark>
+            <v-card-text class="white--text">
+              <div class="headline mb-2">LEADING & DECIDING</div>1. Deciding and Initiating Action
+            </v-card-text>
+
+            <v-card-actions>
+              <v-btn text>Taking Responsibility:</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+
+                <v-col>
+          <v-card color="#385F73" dark>
+            <v-card-text class="white--text">
+              <div class="headline mb-2">SUPPORTING & CO-OPERATING</div>2. Adhering to Principles and Values
+            </v-card-text>
+
+            <v-card-actions>
+              <v-btn text>Taking Responsibility:</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+
+                <v-col>
+          <v-card color="#385F73" dark>
+            <v-card-text class="white--text">
+              <div class="headline mb-2">INTERACTING & PRESENTING</div>3. Relating and Networking
+            </v-card-text>
+
+            <v-card-actions>
+              <v-btn text>Taking Responsibility:</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+
+
+      </v-row>
+    </v-container>
+  </v-card>
 </template>
 
 <script>
 export default {
-    
-}
+  data: () => ({
+      })
+};
 </script>
