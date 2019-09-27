e94501be2377a03de9ec5db4b2908f6be4a44e59
TimEitel <Timothy.Eitel@gmail.com>
2019-09-27 13:28:20 +0800

    added side drawer toggle functionality

--- client/src/components/Toolbar.vue
+++ client/src/components/Toolbar.vue
@@ -1,6 +1,7 @@
 <template>
   <v-card color="grey lighten-4" flat height="200px" tile>
     <v-app-bar app clipped-left class="indigo darken-4">
+      <v-app-bar-nav-icon @click="drawer = !drawer" class="white--text"></v-app-bar-nav-icon>
       <v-toolbar-title class="white--text">
         <!-- <img class="mt-12" :src="require('@/assets/uwa-crest.png')" height="80" /> -->
         Talent Acquisition
@@ -9,22 +10,26 @@
       <div class="flex-grow-1"></div>
 
       <v-toolbar-items>
-        <v-btn text>
-            <v-icon>mdi-home</v-icon>
-            Home</v-btn>
-        <v-btn text>
-            <v-icon>mdi-account-supervisor-outline</v-icon>
-            Competencies</v-btn>
-        <v-btn text>
-            <v-icon>mdi-magnify</v-icon>
-            Search</v-btn>
+        <v-btn text class="white--text">
+          <v-icon>mdi-home</v-icon>Home
+        </v-btn>
+        <v-btn text class="white--text">
+          <v-icon>mdi-account-supervisor-outline</v-icon>Competencies
+        </v-btn>
+        <v-btn text class="white--text">
+          <v-icon>mdi-magnify</v-icon>Search
+        </v-btn>
       </v-toolbar-items>
-
     </v-app-bar>
 
-    <v-navigation-drawer app class="indigo darken-1" clipped dark permanent>
+    <v-navigation-drawer 
+    v-model="drawer" 
+    app 
+    class="indigo darken-1" 
+    clipped 
+    dark>
       <v-list>
-        <v-list-item v-for="comp in competencies" :key="comp.title" @click :to="comp.route">
+        <v-list-item v-for="comp in competencies" :key="comp.title" @click.stop :to="comp.route">
           <v-list-item-icon>
             <v-icon>{{ comp.icon }}</v-icon>
           </v-list-item-icon>
@@ -49,6 +54,7 @@
 export default {
   data() {
     return {
+      drawer: true,
       competencies: [
         { id: "Home", icon: "mdi-home", route: "/" },
         {
