7eda05538d14c3b4d7055708e2583dff9bf16f64
Tim Eitel <Timothy.Eitel@gmail.com>
2019-09-29 19:59:28 +0800

    Fixed double for loop for comp questions

--- client/src/views/Competencies.vue
+++ client/src/views/Competencies.vue
@@ -6,26 +6,16 @@
     <v-layout column wrap>
       <v-flex xs12 md6></v-flex>
       <v-expansion-panels v-model="panel" multiple>
-        <v-expansion-panel>
-          <v-expansion-panel-header>Competency 1</v-expansion-panel-header>
-          <v-expansion-panel-content>
-            <v-checkbox label="Disabled"></v-checkbox>
+        <v-expansion-panel v-for="(comp, index) in competencies" :key="index">
+          <v-expansion-panel-header>{{ comp.id }}</v-expansion-panel-header>
+          <v-expansion-panel-content v-for="question in comp.questions" :key="q">
+            <v-layout row wrap>
+                <v-checkbox :label="question"></v-checkbox>
+            </v-layout>
           </v-expansion-panel-content>
         </v-expansion-panel>
 
-        <v-expansion-panel>
-          <v-expansion-panel-header>Competency 2</v-expansion-panel-header>
-          <v-expansion-panel-content>
-            <v-checkbox label="Disabled"></v-checkbox>
-          </v-expansion-panel-content>
-        </v-expansion-panel>
 
-        <v-expansion-panel>
-          <v-expansion-panel-header>Competency 3</v-expansion-panel-header>
-          <v-expansion-panel-content>
-            <v-checkbox label="Disabled"></v-checkbox>
-          </v-expansion-panel-content>
-        </v-expansion-panel>
       </v-expansion-panels>
     </v-layout>
   </div>
@@ -34,7 +24,22 @@
 <script>
 export default {
   data: () => ({
-    panel: [0, 1]
+    panel: [0, 1],
+        competencies: [
+      {id: "1. Deciding and Initiating Action / Taking Responsibility", questions: ["Give me and example of a time when you did not receive any guidance from your supervisor and had to decide for yourself what to do at work",
+      "Tell me about a time when you asked someone else for help with your job/task.",
+      "Give me an example of a situation in which you had to make a difficult or challenging work-related decision"]},
+      {id: "2. Working with People / Supporting Others", 
+      questions: ["Tell me about the last time you had to help someone at work with his or her job/tasks.",
+      "Tell me about the last time you worked as part of a team/worked with someone else on a task.",
+      "What qualities do you think are necessary to make teams effective."]},
+      {id: "3. Adhering to Principles and Values / Acting with Integrity", questions: ["1", "2","3",]},
+      {id: "4. Relating and Networking / Relating to Others", 
+      questions: ["1", "2","3",]},
+      {id: "5. Presenting and Communicating Information / Oral Communication", questions: ["1", "2","3",]},
+      {id: "6. Writing and Reporting / Written Communication", 
+      questions: ["1", "2","3",]}
+    ]
   })
 };
 </script>
